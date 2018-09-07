c5121c2f4ec27609eb5952df3ca1d86214dbb479
yosiakatsuki <y.ogata@yosiakatsuki.net>
2018-09-07 17:09:13 +0900

    バージョンアップ

--- ys-wp-speech-balloon.php
+++ ys-wp-speech-balloon.php
@@ -6,7 +6,7 @@
  * Author URI:      https://yosiakatsuki.net
  * Text Domain:     ys-wp-speech-balloon
  * Domain Path:     /languages
- * Version:         0.0.1
+ * Version:         0.0.2
  *
  * @package         WP_Speech_Balloon
  */
